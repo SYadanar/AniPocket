688f6409b180f3a25300751b624141d156bfe433
DELL <Keyo-creator>
2021-09-02 21:23:08 +0630

    Hi

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -5,7 +5,7 @@ class ApiService {
   //final String api_key = "d442b4d9-2667-479e-bfb0-5e7ef694664e";
   final String route =
       "https://kitsu.io/api/edge/anime?page%5Blimit%5D=10&page%5Boffset%5D=10";
-Future<AnimeData> getAnime(int page) async {
+Future<AnimeResponse> getResponse() async {
     try {
       final Response response = await Dio().get(
           'https://kitsu.io/api/edge/trending/anime',
@@ -15,7 +15,7 @@ Future<AnimeData> getAnime(int page) async {
       //print("The response is $response");
       //print("The data is ${response.data}");
 
-      return AnimeData.fromJson(response.data);
+      return AnimeResponse.fromJson(response.data);
     } on DioError catch (e) {
       print("The res is " + e.toString());
       throw Exception("The error code is ${e.response?.statusCode}");
